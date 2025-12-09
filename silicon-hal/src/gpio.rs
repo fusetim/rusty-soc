4c7e14be2453d77c2f7068d2d99e9038dcb52260
FuseTim <fusetim.log@gmx.com>
2025-12-09 12:29:15 +0100

    feat: add statefulness to output pin + write mask

--- silicon-hal/src/gpio.rs
+++ silicon-hal/src/gpio.rs
@@ -8,23 +8,29 @@ use crate::{
 };
 use embedded_hal::digital::{ErrorType, InputPin, OutputPin, StatefulOutputPin};
 
-pub trait BankId : Sealed {}
+pub trait BankId: Sealed {}
 
-pub trait BankPinIds {
+pub trait BankPinIds: Sealed {
     type Bank: BankId;
     const BANK_ID: u8;
 }
 
+pub trait IntoPin<I>
+where
+    I: BankPinIds,
+{
+    fn into_pin(self) -> Pin<I>;
+}
+
 pub mod led_bank {
     use crate::{
-        gpio::{BankId, BankPinIds},
+        gpio::{BankId, BankPinIds, IntoPin, Pin},
         typesafe::Sealed,
     };
 
     pub struct LedBank {}
     impl Sealed for LedBank {}
     impl BankId for LedBank {}
-
 
     macro_rules! define_led_pins {
         ($($pin_name:ident => $pin_id:expr),* $(,)?) => {
@@ -37,6 +43,13 @@ pub mod led_bank {
                 impl BankPinIds for $pin_name {
                     type Bank = LedBank;
                     const BANK_ID: u8 = $pin_id;
+                }
+
+                impl IntoPin<$pin_name> for $pin_name {
+                    #[inline(always)]
+                    fn into_pin(self) -> Pin<$pin_name> {
+                        Pin::new_output(self)
+                    }
                 }
             )*
         };
@@ -56,7 +69,7 @@ pub mod led_bank {
 
 pub mod btn_bank {
     use crate::{
-        gpio::{BankId, BankPinIds},
+        gpio::{BankId, BankPinIds, IntoPin, Pin},
         typesafe::Sealed,
     };
 
@@ -77,19 +90,24 @@ pub mod btn_bank {
                     type Bank = BtnBank;
                     const BANK_ID: u8 = $pin_id;
                 }
+
+                impl IntoPin<$pin_name> for $pin_name {
+                    #[inline(always)]
+                    fn into_pin(self) -> Pin<$pin_name> {
+                        Pin::new_input(self)
+                    }
+                }
             )*
         };
     }
 
     define_btn_pins! {
-        Btn0 => 0,
         Btn1 => 1,
         Btn2 => 2,
         Btn3 => 3,
         Btn4 => 4,
         Btn5 => 5,
         Btn6 => 6,
-        Btn7 => 7,
     }
 }
 
@@ -207,14 +225,12 @@ pub struct Gpio {
     led5: Option<led_bank::Led5>,
     led6: Option<led_bank::Led6>,
     led7: Option<led_bank::Led7>,
-    btn0: Option<btn_bank::Btn0>,
     btn1: Option<btn_bank::Btn1>,
     btn2: Option<btn_bank::Btn2>,
     btn3: Option<btn_bank::Btn3>,
     btn4: Option<btn_bank::Btn4>,
     btn5: Option<btn_bank::Btn5>,
     btn6: Option<btn_bank::Btn6>,
-    btn7: Option<btn_bank::Btn7>,
 }
 
 impl Gpio {
@@ -228,18 +244,15 @@ impl Gpio {
             led5: Some(led_bank::Led5 { _inner: () }),
             led6: Some(led_bank::Led6 { _inner: () }),
             led7: Some(led_bank::Led7 { _inner: () }),
-            btn0: Some(btn_bank::Btn0 { _inner: () }),
             btn1: Some(btn_bank::Btn1 { _inner: () }),
             btn2: Some(btn_bank::Btn2 { _inner: () }),
             btn3: Some(btn_bank::Btn3 { _inner: () }),
             btn4: Some(btn_bank::Btn4 { _inner: () }),
             btn5: Some(btn_bank::Btn5 { _inner: () }),
             btn6: Some(btn_bank::Btn6 { _inner: () }),
-            btn7: Some(btn_bank::Btn7 { _inner: () }),
         }
     }
 }
-
 
 macro_rules! gpio_take_led {
     ($($id:expr),*) => {
@@ -273,4 +286,74 @@ macro_rules! gpio_take_btn {
 }
 
 gpio_take_led! {0,1,2,3,4,5,6,7}
-gpio_take_btn! {0,1,2,3,4,5,6,7}
+gpio_take_btn! {1,2,3,4,5,6}
+
+impl Gpio {
+    #[inline(always)]
+    pub fn take_all_btns(
+        &mut self,
+    ) -> Option<(
+        btn_bank::Btn1,
+        btn_bank::Btn2,
+        btn_bank::Btn3,
+        btn_bank::Btn4,
+        btn_bank::Btn5,
+        btn_bank::Btn6,
+    )> {
+        if self.btn1.is_some()
+            && self.btn2.is_some()
+            && self.btn3.is_some()
+            && self.btn4.is_some()
+            && self.btn5.is_some()
+            && self.btn6.is_some()
+        {
+            Some((
+                self.btn1.take().unwrap(),
+                self.btn2.take().unwrap(),
+                self.btn3.take().unwrap(),
+                self.btn4.take().unwrap(),
+                self.btn5.take().unwrap(),
+                self.btn6.take().unwrap(),
+            ))
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn take_all_leds(
+        &mut self,
+    ) -> Option<(
+        led_bank::Led0,
+        led_bank::Led1,
+        led_bank::Led2,
+        led_bank::Led3,
+        led_bank::Led4,
+        led_bank::Led5,
+        led_bank::Led6,
+        led_bank::Led7,
+    )> {
+        if self.led0.is_some()
+            && self.led1.is_some()
+            && self.led2.is_some()
+            && self.led3.is_some()
+            && self.led4.is_some()
+            && self.led5.is_some()
+            && self.led6.is_some()
+            && self.led7.is_some()
+        {
+            Some((
+                self.led0.take().unwrap(),
+                self.led1.take().unwrap(),
+                self.led2.take().unwrap(),
+                self.led3.take().unwrap(),
+                self.led4.take().unwrap(),
+                self.led5.take().unwrap(),
+                self.led6.take().unwrap(),
+                self.led7.take().unwrap(),
+            ))
+        } else {
+            None
+        }
+    }
+}
--- silicon/src/lib.rs
+++ silicon/src/lib.rs
@@ -1,37 +1,92 @@
 #![no_std]
 #![no_main]
-use embedded_hal::digital::OutputPin;
-use silicon_hal::delay::{DelayNs, INTR_DELAY};
+use embedded_hal::digital::{InputPin, OutputPin, StatefulOutputPin};
+use silicon_hal::{
+    delay::{DelayNs, INTR_DELAY},
+    gpio::IntoPin as _,
+};
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-
 #[unsafe(no_mangle)]
 fn main() -> ! {
-    use silicon_hal::gpio::{Pin};
-
     let mut peripherals = silicon_hal::init();
 
-    let mut led0 = Pin::new_output(peripherals.gpio.take_led0().unwrap());
-    let mut led1 = Pin::new_output(peripherals.gpio.take_led1().unwrap());
+    let (mut led0, mut led1, mut led2, mut led3, mut led4, mut led5, mut led6, mut led7) = {
+        let (led0, led1, led2, led3, led4, led5, led6, led7) =
+            peripherals.gpio.take_all_leds().unwrap();
+        (
+            led0.into_pin(),
+            led1.into_pin(),
+            led2.into_pin(),
+            led3.into_pin(),
+            led4.into_pin(),
+            led5.into_pin(),
+            led6.into_pin(),
+            led7.into_pin(),
+        )
+    };
+    let (mut btn1, mut btn2, mut btn3, mut btn4, mut btn5, mut btn6) = {
+        let (btn1, btn2, btn3, btn4, btn5, btn6) = peripherals.gpio.take_all_btns().unwrap();
+        (
+            btn1.into_pin(),
+            btn2.into_pin(),
+            btn3.into_pin(),
+            btn4.into_pin(),
+            btn5.into_pin(),
+            btn6.into_pin(),
+        )
+    };
 
-    let mut count : u8 = 0;
+    let mut count: u8 = 0;
     loop {
-        if count % 2 == 0 {
-            led0.set_high();
+        // Read buttons and set corresponding LEDs
+        if btn1.is_high().unwrap() {
+            led1.set_high().unwrap();
         } else {
-            led0.set_low();
+            led1.set_low().unwrap();
         }
-        if count % 4 == 0 {
-            led1.set_high();
+        if btn2.is_high().unwrap() {
+            led2.set_high().unwrap();
         } else {
-            //led1.set_low();
+            led2.set_low().unwrap();
         }
+        if btn3.is_high().unwrap() {
+            led3.set_high().unwrap();
+        } else {
+            led3.set_low().unwrap();
+        }
+        if btn4.is_high().unwrap() {
+            led4.set_high().unwrap();
+        } else {
+            led4.set_low().unwrap();
+        }
+        if btn5.is_high().unwrap() {
+            led5.set_high().unwrap();
+        } else {
+            led5.set_low().unwrap();
+        }
+        if btn6.is_high().unwrap() {
+            led6.set_high().unwrap();
+        } else {
+            led6.set_low().unwrap();
+        }
+
+        // Toggle led0/led7 every 8 cycles
+        if count % 8 == 0 {
+            led0.toggle().unwrap();
+        } else if count % 8 == 4 {
+            led7.toggle().unwrap();
+        }
+
+        // Simple delay
+        delay_ms(125);
+
+        // Increment count (not used for now)
         count = count.wrapping_add(1);
-        delay_ms(1000);
     }
 }
 
